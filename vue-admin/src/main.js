29167c3aa59d2b747f98a2cff6cfe488abfd5ff9
wanghao1012 <wang20021012hao@163.com>
2023-08-13 11:03:24 +0800

    智慧园区项目搭建完毕

---
+++ vue-admin/src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+// 初始化样式
+import 'normalize.css/normalize.css'
+// 引入ElementUI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import '@/styles/index.scss' // global css
+
+// 引入根组件
+import App from './App'
+// 引入Vuex
+import store from './store'
+// 引入Router路由
+import router from './router'
+
+// 注册svg
+import '@/icons'
+// 把ElementUI提供的所有组件注册为全局组件
+Vue.use(ElementUI)
+
+Vue.config.productionTip = false
+
+// vue实例化
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
